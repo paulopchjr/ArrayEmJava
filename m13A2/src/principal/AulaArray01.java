4a74d4aa580890c8da3ed67f381f05d6d9a7f3e5
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-11-13 14:52:27 -0300

    CRIANDO O PRIMEIRO ARRAY EM JAVA

---
+++ m13A2/src/principal/AulaArray01.java
@@ -0,0 +1,21 @@
+package principal;
+
+public class AulaArray01 {
+
+	public static void main(String[] args) {
+
+		/* Array pode ser todos os tipos de dados e objetos tambem */
+		/* Array sempre deve ter a quantidade de posicoes definidas */
+
+		double[] notas = new double[4];
+
+		notas[0] = 7.5;
+		notas[1] = 8.0;
+		notas[2] = 8.5;
+		notas[3] = 9.0;
+		
+		System.out.println(notas);
+
+	}
+
+}
