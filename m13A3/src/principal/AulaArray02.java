281ac230a537c8bdaea0d3a353bb06666c8247ab
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2023-11-13 14:58:13 -0300

    RECUPERANDO OS VALORES DO ARRAY

---
+++ m13A3/src/principal/AulaArray02.java
@@ -0,0 +1,26 @@
+package principal;
+
+public class AulaArray02 {
+	
+	public static void main(String[] args) {
+		
+		double[] notas = new double[4];
+		
+		
+		notas[0] = 10;
+		notas[1] = 9.5;
+		notas[2] = 9;
+		notas[3] = 8.5;
+
+	
+		
+		/*Recuperando os valores do array*/
+		System.out.println("1º Posição do array é  0, o seu valor é = " + notas[0] );
+		System.out.println("2º Posição do array é  1, o seu valor é = " + notas[1] );
+		System.out.println("3º Posição do array é  2, o seu valor é = " + notas[2] );
+		System.out.println("4º Posição do array é  3, o seu valor é = " + notas[3] );
+		
+		
+	}
+
+}
